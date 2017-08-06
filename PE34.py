b7b4595ea605021636f8b8d1d486e621807642c7
Sophie Tan <sophiemltan@gmail.com>
2017-08-06 20:13:53 +0100

    Adjusted for pep8/257

--- PE34.py
+++ PE34.py
@@ -1,19 +1,17 @@
 """Solution for Problem 34 on Project Euler."""
 
-
 from math import factorial as fac
 
+
 def fact_digit():
-    """
-    Sum all numbers who's digit factorial sum is the original number.
-    """
+    """Sum all numbers who's digit factorial sum is the original number."""
     f = {str(x): fac(x) for x in range(10)}
     result = []
-    
+
     for n in range(50000):
         n_sum = sum(f[digit] for digit in str(n))
         if n_sum == n:
-            result.append(n)           
+            result.append(n)
     return sum(result)
 
 print(fact_digit())
